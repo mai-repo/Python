b68dfb2e2eb459cf89f14cb7eeec8ed1b8e431f8
mai-repo <ttmai@my.loyno.edu>
2024-12-18 13:56:45 -0500

    adding in files to this new repo

---
+++ First_Assignment.py
@@ -0,0 +1,15 @@
+# As a beginner Python learner, your task is to demonstrate that you can set up Python locally and create simple unit tests. Follow these steps:
+
+    # Install Python on your local machine if you haven't already. Make sure you have version 3.x installed.
+    # Open a text editor or IDE (like PyCharm, VSCode, or Sublime Text) and create a new Python file.
+    # Write a simple Python script with at least two functions that perform different operations. For example:
+
+def add_numbers(a, b):
+  return a + b
+
+print (add_numbers(2,3))
+
+def multiply_numbers(a, b):
+    return a * b
+
+print (multiply_numbers(2,3))
