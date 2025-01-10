cf708b6f171b15e97c829c6b2332dd05ed013b6f
mai-repo <ttmai@my.loyno.edu>
2025-01-09 20:52:03 -0500

    edited HTML to add in a link to the data as well as download tailwindcss for styling purposes

--- Week4/process_and_upload.py
+++ Week4/process_and_upload.py
@@ -1,9 +1,10 @@
 import os
 import requests # This is a third party library that you need to install to request HTTP requests
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 import json
 
 app = Flask(__name__)
+file = '/Users/thanhmai/Python_Assignments/Week4/user_data.json'
 
 def process_file(file_path):
     try:
@@ -14,7 +15,7 @@ def process_file(file_path):
     except Exception as e:
         print(f"An error occured: {e}")
         return None 
-
+# This function will upload the data to the web service
 def upload_to_web_service(data):
     try:
         response = requests.post("http://localhost:5000/feedback", json=data)
@@ -23,6 +24,19 @@ def upload_to_web_service(data):
         print(f"An error occured: {e}")
         return None
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/file', methods=['GET'])
+def get_feedback():
+    try:
+        return process_file(file)
+    except Exception as e:
+        print(f"An error occured: {e}")
+        return None
+
+# This is the route that will be used to upload the file
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['file']
@@ -37,7 +51,7 @@ def upload_file():
     else:
         return jsonify({"message": "An error occured while processing the file"}), 500
 
-if __name__= "__main__":
+if __name__== "__main__":
     app.run(debug=True, port=5000) # Run the app in debug mode and on port 5000
 
     
